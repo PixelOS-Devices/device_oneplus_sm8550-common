20165a8d2069fd1dd6f08376811799481fcb6741
inferno0230 <mail@inferno0230.in>
2024-02-07 13:15:00 +0530

    sm8550-common: Add aston to build guards

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter salami,$(TARGET_DEVICE)),)
+ifneq ($(filter salami aston,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
