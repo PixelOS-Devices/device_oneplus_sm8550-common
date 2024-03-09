c5e201525dc7e368fbd6c8c2e1712f79d8c1ff5f
inferno0230 <mail@inferno0230.in>
2024-03-09 23:20:36 +0530

    sm8550-common: Add camera libs/packages required by aston
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -107,9 +107,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.stats-V1-ndk.vendor \
     android.hardware.camera.common@1.0.vendor \
+    android.hardware.camera.common-V1-ndk.vendor \
     android.hardware.camera.device-V1-ndk.vendor \
     android.hardware.camera.metadata-V1-ndk.vendor \
     android.hardware.camera.provider-V1-ndk.vendor \
+    android.frameworks.cameraservice.common-V1-ndk.vendor \
+    android.frameworks.cameraservice.service-V1-ndk.vendor \
     vendor.qti.hardware.camera.aon@1.3.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
