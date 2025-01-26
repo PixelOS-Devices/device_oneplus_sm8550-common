b246f9fffd051ed077e8dace1f5c123f888249f8
Bruno Martins <bgcngm@gmail.com>
2025-01-26 13:15:37 +0000

    sm8550-common: Build more audio blobs from sources
    Change-Id: I6c05826778c05ef14f7e4fbdbf40a1cd73e68bf5

--- extract-files.py
+++ extract-files.py
@@ -71,8 +71,13 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.1',
     ): lib_fixup_vendor_suffix,
     (
+        'libar-acdb',
+        'libar-gsl',
+        'liblx-osal',
+        'libats',
         'libagmclient',
         'libpalclient',
+        'vendor.qti.hardware.AGMIPC@1.0-impl',
     ): lib_fixup_remove,
 }
 
