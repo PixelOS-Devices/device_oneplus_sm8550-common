275be456dfa8064e618fa634c50656bb4f52a1af
Bruno Martins <bgcngm@gmail.com>
2025-02-11 00:16:27 +0000

    sm8550-common: Drop prebuilt IOplusTouch
    The interface has been added to hardware/oplus.
    
    Change-Id: Ieb9b66d9ccb878d1a3cc4c51361a22499153107b

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,6 @@ lib_fixups: lib_fixups_user_type = {
     (
         'libpwirisfeature',
         'libpwirishalwrapper',
-        'vendor.oplus.hardware.touch-V1-ndk',
         'vendor.oplus.hardware.commondcs-V1-ndk'
     ): lib_fixup_odm_suffix,
     (
