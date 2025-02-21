f6e429067cbd6766fe4dc2684742617470cd1490
inferno0230 <mail@inferno0230.in>
2025-02-21 17:39:41 +0000

    sm8550-common: Add odm suffix to vendor.oplus.hardware.commondcs-V1-ndk
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,7 @@ lib_fixups: lib_fixups_user_type = {
         'libpwirisfeature',
         'libpwirishalwrapper',
         'vendor.oplus.hardware.touch-V1-ndk',
+        'vendor.oplus.hardware.commondcs-V1-ndk'
     ): lib_fixup_odm_suffix,
     (
         'com.qualcomm.qti.dpm.api@1.0',
